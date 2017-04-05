05399301bc4c43ee2da56fc93ccbadd1357a20e4
Brandon Hill <bhill010@gmail.com>
2017-04-05 10:44:00 -0700

    Initialize with redux boilerplate and sample reducer

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './reducers';
+
+class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: "AIzaSyBET8U0TQP05EVk5kRWs01qHFg_e0P0sKU",
+      authDomain: "manager-a3167.firebaseapp.com",
+      databaseURL: "https://manager-a3167.firebaseio.com",
+      projectId: "manager-a3167",
+      storageBucket: "manager-a3167.appspot.com",
+      messagingSenderId: "1073589556268"
+  };
+
+  firebase.initializeApp(config);
+  }
+  
+  render() {
+    return (
+      <Provider store={createStore(reducers)}>
+        <View>
+          <Text>
+            Hello!
+          </Text>
+        </View>
+      </Provider>
+    );
+  }
+}
+
+export default App;
