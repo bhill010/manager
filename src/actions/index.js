80b4fe5c314a951656dcd06edd8db807d6bb1884
Brandon Hill <bhill010@gmail.com>
2017-04-05 15:10:18 -0700

    Add user login success to auth

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+import firebase from 'firebase';
 import {
   EMAIL_CHANGED,
   PASSWORD_CHANGED
@@ -14,5 +15,18 @@ export const passwordChanged = (text) => {
   return {
     type: PASSWORD_CHANGED,
     payload: text
+  };
+};
+
+export const loginUser = ({ email, password }) => {
+  //dispatch comes from redux-thunk. used for asynchronous actions -- this action
+  //makes a call to database that takes time, so we have to handle this asynchronous
+  //call with redux-thunk
+  return (dispatch) => {
+    // make request to firebase server to attempt user login
+    firebase.auth().signInWithEmailAndPassword(email, password)
+    .then(user => {
+      dispatch({ type: 'LOGIN_USER_SUCCESS', payload: user });
+    });
   };
 };
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { emailChanged, passwordChanged } from '../actions';
+import { emailChanged, passwordChanged, loginUser } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
 
 class LoginForm extends Component {
@@ -12,6 +12,12 @@ class LoginForm extends Component {
 
   onPasswordChange(text) {
     this.props.passwordChanged(text);
+  }
+
+  onButtonPress() {
+    const { email, password } = this.props;
+
+    this.props.loginUser({ email, password });
   }
 
   render() {
@@ -37,7 +43,7 @@ class LoginForm extends Component {
         </CardSection>
 
         <CardSection>
-          <Button>
+          <Button onPress={this.onButtonPress.bind(this)}>
             Login
           </Button>
         </CardSection>
@@ -53,6 +59,8 @@ const mapStateToProps = state => {
   };
 };
 
-export default connect(mapStateToProps, { emailChanged, passwordChanged })(LoginForm);
+export default connect(mapStateToProps, {
+  emailChanged, passwordChanged, loginUser
+})(LoginForm);
 // emailChanged is the action creator we want to bind to our component,
 // which can be put into the second argument of the connect function
